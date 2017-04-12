6516142b5ae4b3c1ccdcd59d73576937cf3e9c43
Binay Mishra <binaymishra@gmail.com>
2017-04-12 15:39:42 +0530

    clean code using 'lombok'

--- src/main/java/com/tracker/TrackerApplication.java
+++ src/main/java/com/tracker/TrackerApplication.java
@@ -1,26 +1,98 @@
 package com.tracker;
 
+import java.io.File;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ResourceLoader;
 
 @SpringBootApplication
 public class TrackerApplication{
 	
-	@Bean
-	CommandLineRunner runApplication(TrackerRepository trackerRepository){
-		return args ->{
-			System.out.println("======================================Table A======================================");
-			trackerRepository.insertTableA();
-			System.out.println("======================================Table B======================================");
-			trackerRepository.insertTableB();
-			System.out.println("======================================Table C======================================");
-			trackerRepository.insertTableC();
-		};
-	}
+	private static final String COMMA = ",";
+
+	@Value("${tableAFile.path}")
+	private String tableAFile;
+	
+	@Value("${tableBFile.path}")
+	private String tableBFile;
+	
+	@Value("${tableCFile.path}")
+	private String tableCFile;
+	
+	@Autowired
+	ResourceLoader resourceLoader;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrackerApplication.class, args);
+	}
+	
+	@Bean
+	CommandLineRunner runApplication(TrackerRepository trackerRepository){
+		return args ->{
+			File file;
+			System.out.println("======================================Table A======================================");
+			file = resourceLoader.getResource(tableAFile).getFile();
+			List<TableA> tableA = Files.lines(file.toPath(), Charset.forName("UTF-8"))
+					.skip(1)
+					.map(str -> {
+						String[] arr = str.split(COMMA);
+						return  new TableA(arr[0], arr[1], arr[2], arr[3], Long.parseLong(arr[4]), Long.parseLong(arr[5])) ;
+					}).collect(Collectors.toList());
+			//Insert into table_a
+			trackerRepository.insertTableA(tableA);
+			
+			System.out.println("======================================Table B======================================");
+			file = resourceLoader.getResource(tableBFile).getFile();
+			List<TableB> tableB = Files.lines(file.toPath(), Charset.forName("UTF-8"))
+					.skip(1)
+					.map(str -> {
+						String[] arr = str.split(COMMA);
+						return  new TableB(arr[0], arr[1], arr[2], arr[3], Long.parseLong(arr[4]), Long.parseLong(arr[5])) ;
+					}).collect(Collectors.toList());
+			//Insert into table_b
+			trackerRepository.insertTableB(tableB);
+			
+			System.out.println("======================================Table C======================================");
+			file = resourceLoader.getResource(tableCFile).getFile();
+			List<TableC> tableC = Files.lines(file.toPath(), Charset.forName("UTF-8"))
+			.skip(1)
+			.map(str ->{
+				String[] arr = str.split(COMMA);
+				return new TableC(arr[0], arr[3], arr[4], Double.valueOf(arr[1]), Double.valueOf(arr[2])); 
+			//arr[0], arr[3], arr[4], Double.valueOf(arr[1]), Double.valueOf(arr[2])
+			}).collect(Collectors.toList());
+			//Insert into table_c
+			trackerRepository.insertTableC(tableC);
+		};
+	}
+	
+	
+	@Data @NoArgsConstructor @AllArgsConstructor
+	class TableA {
+		String userIp, userMac, apName, apMac;
+		Long startTime, endTime;
+	}
+	@Data @NoArgsConstructor @AllArgsConstructor
+	class TableB {
+		String userIp, userMac, apName, apMac;
+		Long startTime, endTime;
+	}
+	@Data @NoArgsConstructor @AllArgsConstructor
+	class TableC {
+		String prefix, name, category;
+		Double lat, lon;
 	}
 }
--- src/main/java/com/tracker/TrackerRepository.java
+++ src/main/java/com/tracker/TrackerRepository.java
@@ -1,6 +1,5 @@
 package com.tracker;
 
-import java.io.IOException;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
@@ -12,6 +11,10 @@ import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.tracker.TrackerApplication.TableA;
+import com.tracker.TrackerApplication.TableB;
+import com.tracker.TrackerApplication.TableC;
 
 
 @Repository
@@ -30,59 +33,53 @@ public class TrackerRepository {
 		template = new JdbcTemplate(dataSource);
 	}
 	
-	@Autowired
-	TrackerComponent trackerComponent;
-	
-	public void insertTableA() throws IOException{
-		List<TableA> tableA = trackerComponent.readFileTableA20120407();
+	public void insertTableA(final List<TableA> tableA) {
 		int[] rows = template.batchUpdate(tableAinsert, new BatchPreparedStatementSetter() {
-				
-				@Override
-				public void setValues(PreparedStatement ps, int i) throws SQLException {
-					TableA a = tableA.get(i);
-					ps.setString(1, a.getUserIp());
-					ps.setString(2, a.getUserMac());
-					ps.setString(3, a.getApName());
-					ps.setString(4, a.getApMac());
-					ps.setLong(5, a.getStartTime());
-					ps.setLong(6, a.getEndTime());
-					
-				}
-				
-				@Override
-				public int getBatchSize() {
-					return tableA.size();
-				}
-			});
-		System.out.println(String.format("%d rows inserted in tableA ", rows.length));
+
+			@Override
+			public void setValues(PreparedStatement ps, int i) throws SQLException {
+				TableA a = tableA.get(i);
+				ps.setString(1, a.getUserIp());
+				ps.setString(2, a.getUserMac());
+				ps.setString(3, a.getApName());
+				ps.setString(4, a.getApMac());
+				ps.setLong(5, a.getStartTime());
+				ps.setLong(6, a.getEndTime());
+
+			}
+
+			@Override
+			public int getBatchSize() {
+				return tableA.size();
+			}
+		});
+		System.err.println(String.format("%d rows inserted in 'table_a'.", rows.length));
 	}
 	
-	public void insertTableB() throws IOException{
-		List<TableB> tableB = trackerComponent.readFileTableB20120409();
-			int[] rows = template.batchUpdate(tableBinsert, new BatchPreparedStatementSetter() {
-				
-				@Override
-				public void setValues(PreparedStatement ps, int i) throws SQLException {
-					TableB  b = tableB.get(i);
-					ps.setString(1, b.getUserIp());
-					ps.setString(2, b.getUserMac());
-					ps.setString(3, b.getApName());
-					ps.setString(4, b.getApMac());
-					ps.setLong(5, b.getStartTime());
-					ps.setLong(6, b.getEndTime());
-					
-				}
-				
-				@Override
-				public int getBatchSize() {
-					return tableB.size();
-				}
-			});
-			System.out.println(String.format("%d rows inserted in tableB ", rows.length));
+	public void insertTableB(final List<TableB> tableB) {
+		int[] rows = template.batchUpdate(tableBinsert, new BatchPreparedStatementSetter() {
+
+			@Override
+			public void setValues(PreparedStatement ps, int i) throws SQLException {
+				TableB b = tableB.get(i);
+				ps.setString(1, b.getUserIp());
+				ps.setString(2, b.getUserMac());
+				ps.setString(3, b.getApName());
+				ps.setString(4, b.getApMac());
+				ps.setLong(5, b.getStartTime());
+				ps.setLong(6, b.getEndTime());
+
+			}
+
+			@Override
+			public int getBatchSize() {
+				return tableB.size();
+			}
+		});
+		System.err.println(String.format("%d rows inserted in 'table_b'.", rows.length));
 	}
 	
-	public void insertTableC() throws IOException{
-		List<TableC> tableC = trackerComponent.readFileTableC();
+	public void insertTableC(List<TableC> tableC) {
 		int[] rows = template.batchUpdate(tableCinsert, new BatchPreparedStatementSetter() {
 
 			@Override
@@ -93,17 +90,16 @@ public class TrackerRepository {
 				ps.setDouble(3, c.getLon());
 				ps.setString(4, c.getName());
 				ps.setString(5, c.getCategory());
-				
+
 			}
 
 			@Override
 			public int getBatchSize() {
 				return tableC.size();
 			}
-			
+
 		});
-		
-		System.out.println(String.format("%d rows inserted in tableC ", rows.length));
+		System.err.println(String.format("%d rows inserted in 'table_c'.", rows.length));
 	}
 
 }
